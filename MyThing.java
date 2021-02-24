98a9c47d2c363e89f5f055cfe900a9b52ba30d38
Sherryll Elliott <sherryllelliott@gmail.com>
2021-02-24 19:17:49 +0000

    data types and operators

---
+++ MyThing.java
@@ -0,0 +1,17 @@
+
+//! press F5 to run Java in VS Code. 
+//public means its available to other parts of the program
+//class is a big storage box you can put anything into
+
+public class MyThing
+{
+  //entry point
+  //static "only one of these things can ever exist" - shows where to start the program
+  //File has to be named after the class.
+public static void main (String[]args){
+  System.out.println("hello!");
+  }
+
+}
+
+
---
+++ Variables.java
@@ -0,0 +1,27 @@
+public class Variables {
+  
+    public static void main (String []args){
+      //declare variables:
+      int a = 9;
+      // decimal points:
+      double c = 9.87;
+      //F at the end
+      float d = 9.86f;
+
+      boolean e = false;
+
+      String f = "hello this is a string";
+
+      
+
+      //OPERATORS
+
+      int g = 10;
+      int h = 11;
+
+      System.out.println(g+h);
+
+    }
+
+  
+}
