33425cc56470e1bb1d89d647282a8a7895dbea83
Sherryll Elliott <sherryllelliott@gmail.com>
2021-02-25 09:45:46 +0000

    methods added

---
+++ methods.java
@@ -0,0 +1,13 @@
+public class methods {
+
+  // methods seem to just be functions.
+
+  public static void addNumbers (int a, int b){
+
+    int result = a + b;
+
+    System.out.println(result);
+
+  }
+  
+}
